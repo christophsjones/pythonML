2fe3b732674f6efebad49f7b250a875b904c31f4
Chris Jones <csj@andrew.cmu.edu>
2014-07-26 14:23:30 -0700

    Fixed syntax

--- neuralnets.py
+++ neuralnets.py
@@ -10,17 +10,26 @@ class NN(object):
 
   def __init__(self, n_in, n_out, *hidden):
     # Initialize size of each layer
-    self.layers = array((n_in,) + hidden + (n_out,))
-  
+    # One extra input for bias
+    self.layers = array((n_in + 1,) + hidden + (n_out,))
+    self.n_layers = len(self.layers)
+
     # Initialize activation values for each node
     # Could use numpy array, but they're updated sequentially anyway
-    self.activations = [ones(length) for length in self.layers]
+    self.activations = [ones(node_count) for node_count in self.layers]
     
     # Initialize weights between levels
     self.weights = [random.uniform(-0.5, 0.5, (i,j)) for i,j in zip(self.layers[:-1], self.layers[1:])]
 
-  def activate(self):
+    # Initialize previous change in weights, for momentum
+    self.change_weights = [zeros([i,j]) for i,j in zip(self.layers[:-1], self.layers[1:])]
+
+  def activate(self, input):
     """Activate all nodes, and output last layer"""
+    self.activations[0] = input
+    for i in xrange(self.n_layers - 1):
+      self.activations[i + 1] = self.weights.dot(self.activations[i])
+
     return random.uniform(-0.5, 0.5, n_out)
 
   def backPropagate(self, targets, N, M):
@@ -30,4 +39,4 @@ class NN(object):
   def train(self, data, targets):
     """Trains the neural network"""
     for i in range(num_epochs):
-      self.backPropagate(targets, 0.1, 0.01):
+      self.backPropagate(targets, 0.1, 0.01)
