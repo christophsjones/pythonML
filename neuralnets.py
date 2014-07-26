011d0cc7f05b056da549eb64eb48e63fb9adc634
Chris Jones <csj@andrew.cmu.edu>
2014-07-26 13:51:55 -0700

    Added functionality

--- neuralnets.py
+++ neuralnets.py
@@ -11,4 +11,10 @@ class NN(object):
   def __init__(self, n_in, n_out, *hidden):
     # Initialize size of each layer
     self.layers = array((n_in,) + hidden + (n_out,))
+  
+    # Initialize activation values for each node
+    # Could use numpy array, but they're updated sequentially anyway
+    self.activations = [ones(length) for length in self.layers]
     
+    # Initialize weights between levels
+    self.weights = [random.uniform(-0.5, 0.5, (i,j)) for i,j in zip(self.layers[:-1], self.layers[1:])]
