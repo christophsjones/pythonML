bf8b9ed9b41948578ae0b9473cdb317af09880f8
Chris Jones <csj@andrew.cmu.edu>
2014-08-03 23:59:24 -0700

    Fixed derivative calculation

--- neuralnets.py
+++ neuralnets.py
@@ -9,11 +9,11 @@ from numpy import *
 # Sigmoid function used in this neural network
 def sigmoid(xs):
   """Compute sigmoid at each x in vector"""
-  return 1.0 / (1.0 + exp(-xs))
+  return 2.0 / (1.0 + exp(-xs)) - 1.0
 
 def dsigmoid(ys):
   """Compute sigmoid derivative from each y in vector"""
-  return ys * (1.0 - ys)
+  return 0.5 * (1.0 - ys*ys)
 
 class NN(object):
 
@@ -21,7 +21,7 @@ class NN(object):
     hidden, n_out = hidden[:-1], hidden[-1]
     # Initialize size of each layer
     # One extra input for biases
-    self.layers = array([node_count + 1 for node_count in (n_in,) + hidden] + [n_out])
+    self.layers = array((n_in,) + hidden + (n_out,))
     self.n_layers = len(self.layers)
 
     # Initialize activation values for each node
@@ -29,53 +29,42 @@ class NN(object):
     self.activations = [ones(node_count) for node_count in self.layers]
     
     # Initialize weights between levels, and past changes for momentum
-    self.bias_correct = ~identity(self.n_layers - 1, dtype=bool)[-1]
-    self.weights = [random.uniform(-0.5, 0.5, [i, j]) for i,j in zip(self.layers[1:] - self.bias_correct, self.layers[:-1])]
-    self.past_change = [zeros([i, j]) for i,j in zip(self.layers[1:] - self.bias_correct, self.layers[:-1])]
-
-    self.input_scale = (ones(n_in), zeros(n_in))
-    self.output_scale = (ones(n_out), zeros(n_out))
+    self.weight_shapes = zip(self.layers[1:], self.layers[:-1])
+    
+    self.weights = [random.uniform(-0.5, 0.5, shape) for shape in self.weight_shapes]
+    self.past_change = [zeros(shape) for shape in self.weight_shapes]
 
   def activate(self, inputs):
     """Activate all nodes, and output last layer"""
-    inputs = (inputs - self.input_scale[1]).astype(float) / self.input_scale[0]
+    self.activations[0] = inputs
 
-    ans = self._activate(inputs) * self.output_scale[0] + self.output_scale[1]
-    # Return a copy of the activation value for modification
-    return ans[:]
-
-  def _activate(self, inputs):
-    """Internal activation, does not scale input/output"""
-    # Append bias term
-    self.activations[0] = append(inputs, 1.0)
+    # Append bias term, and don't use sigmoid on input
+    self.activations[1] = self.weights[0].dot(append(self.activations[0], 1.0))
 
     # Activate each hidden layer, using sigmoid function
-    for i in xrange(self.n_layers - 2):
-      self.activations[i + 1] = append(sigmoid(self.weights[i].dot(self.activations[i])), 1.0)
-    
-    # Activate output layer, don't use sigmoid function
-    # Has the advantage of simulating a perceptron when there are no hidden layers
-    self.activations[-1] = self.weights[-1].dot(self.activations[-2])
+    for i in xrange(1, self.n_layers - 1):
+      self.activations[i + 1] = self.weights[i].dot(append(sigmoid(self.activations[i]), 1.0))
 
-    return self.activations[-1]
+    return self.activations[-1][:]
 
   def backPropagate(self, targets, a, M):
     """Update weights assuming neural network is activated to achieve targets""" 
 
-    current_change = [zeros([i,j]) for i,j in zip(self.layers[1:] - self.bias_correct, self.layers[:-1])]
+    current_change = [zeros(shape) for shape in self.weight_shapes]
     
     error = self.activations[-1] - targets
-    # The actual cost function being minimized
-    err = 0.5 * dot(error,error.conj())
+    # Return square error
+    err = dot(error, error) 
+    deltas = error
 
-    deltas = append(error, 0.0)
-    current_change[-1] = outer(error, self.activations[-2])
 
-    for i in reversed(xrange(self.n_layers - 2)):
-      error = self.weights[i + 1].T.dot(deltas[:-1])
-      deltas = dsigmoid(self.activations[i + 1]) * error
-
-      current_change[i] = outer(deltas[:-1], self.activations[i])
+    for i in reversed(xrange(1, self.n_layers - 1)):
+      current_change[i] = outer(deltas, append(sigmoid(self.activations[i]), 1.0))
+      
+      error = self.weights[i].T.dot(deltas)
+      deltas = dsigmoid(self.activations[i]) * error[:-1]
+    
+    current_change[0] = outer(deltas, append(self.activations[0], 1.0))
 
     for i in xrange(self.n_layers - 1):
       self.weights[i] -= M*self.past_change[i] + a*current_change[i]
@@ -83,30 +72,24 @@ class NN(object):
 
     return err
 
-  def train(self, data, targets, num_epochs=1000, a=0.01, M=0.002, verbose=True):
+  def train(self, data, targets, num_epochs=1000, a=0.02, M=0.002, e=0.000001, verbose=True):
     """Trains the neural network"""
-    
-    # Scale inputs to [-1,1]
-    # Could use log scale if there are outliers
-    u,v = amax(data, 0), amin(data, 0)
-    w = u - v
-    w[w == 0.0] = 2.0
-    self.input_scale = (0.5 * w, 0.5 * (u + v))
-    data = (data - self.input_scale[1]).astype(float) / self.input_scale[0]
-    print "Input scale:", self.input_scale
 
-    # If amax == amin, GG
-    u,v = amax(targets, 0), amin(targets, 0)
-    self.output_scale = (0.5 * (u - v), 0.5 * (u + v))
-    targets = (targets - self.output_scale[1]).astype(float) / self.output_scale[0]
-    print "Output scale:", self.output_scale
-
+    past_error = -2 * e
     error = 0.0
     for i in xrange(num_epochs):
       error = 0.0
       for x,y in zip(data, targets):
         self.activate(x)
         error += self.backPropagate(y, a, M)
-      if verbose and i % (num_epochs / 10) == 0:
-        print "Iteration: %s of %s, Error: %s" % (i,num_epochs,error)
-    return error
+      if verbose and i % max(num_epochs / 10, 1) == 0:
+        print "Iteration: %s of %s, Error: %s" % (i,num_epochs, (error / len(data)) ** 0.5)
+      
+      if abs(past_error - error) <= 0:
+        if verbose:
+          print "Converged on iteration %s of %s, Error: %s" % (i, num_epochs, (error / len(data)) ** 0.5)
+        break
+      past_error = error
+
+    # Return RMS error
+    return (error / len(data)) ** 0.5
