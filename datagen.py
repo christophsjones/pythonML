220bc3aec0210ed801eb2c215b0248349033ccc4
Chris Jones <csj@andrew.cmu.edu>
2014-08-03 23:59:45 -0700

    Fixed derivative calculation

--- datagen.py
+++ datagen.py
@@ -4,11 +4,25 @@ def linear(m=200, n=10):
   xs = random.rand(m, n)
   coeff = 2 * random.rand(n) - 1
   ys = xs.dot(coeff)
-  return {"data" : xs, "target" : ys, "coeff" : coeff, "dim" : (n,1)}
+  return {"data" : xs, "target" : ys, "dim" : (n,1), "coeff" : coeff}
 
 def linear_approx(m=200, n=10, s=0.05):
   xs = random.rand(m,n)
   coeff = 2 * random.rand(n) - 1
   ys = xs.dot(coeff) + s * random.randn(m)
-  return {"data" : xs, "target" : ys, "coeff" : coeff, "dim" : (n,1)}
+  return {"data" : xs, "target" : ys, "dim" : (n,1), "coeff" : coeff}
+
+def fromfunc(f=lambda x,y: x**2 + y**2, m=200, n=2):
+  xs = random.rand(m,n)
+  ys = zeros(m)
+  for i in xrange(m):
+    ys[i] = f(xs[i])
+  return {"data" : xs, "target" : ys, "dim" : (n,1)}
+
+def fromfunc(f=lambda x,y: x**2 + y**2, m=200, n=2, s=0.05):
+  xs = random.rand(m,n)
+  ys = s * random.randn(m)
+  for i in xrange(m):
+    ys[i] += f(xs[i])
+  return {"data" : xs, "target" : ys, "dim" : (n,1)}
 
